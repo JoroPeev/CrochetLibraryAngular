ac5587ae0aa8a5ac35251bdea81c1c479de7a3a5
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-09 18:47:24 +0300

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+// Import your home and shop components
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
