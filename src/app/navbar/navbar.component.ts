710880fe57c67a152aa1ad89a70714aed6177986
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-09 18:57:53 +0300

    Navbar corrections

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+// navbar.component.ts
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  standalone: true
+})
+export class NavbarComponent {
+  @Output() pageChange = new EventEmitter<string>();
+  
+  navigate(page: string) {
+    this.pageChange.emit(page);
+  }
+}
