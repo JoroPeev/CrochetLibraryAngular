529754460c2d8928ba464231e890bda4110e2048
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-15 23:15:51 +0300

    Added about component to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
-// app.routes.ts
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent }
 ];
