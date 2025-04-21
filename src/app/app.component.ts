5358f9b557519dbda8e43617c9c0b26cc8348fbf
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-21 09:27:17 +0300

    Added api settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // <-- Import this
+import { ApiService } from './services/api.service'; // <-- Make sure ApiService is imported
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './about/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, NavbarComponent],
+  imports: [
+    RouterModule,
+    NavbarComponent,
+    CommonModule,
+    HttpClientModule 
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  featuredToys: any;
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getToys().subscribe({
+      next: (res) => {
+        console.log('Backend response:', res);
+        this.featuredToys = res;
+      },
+      error: (err) => console.error('API Error:', err)
+    });
+  }
+}
