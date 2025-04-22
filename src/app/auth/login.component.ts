6aade8a39675d54e2ec17afa09e8c956b1aacf9a
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-22 20:09:29 +0300

    Added login component

---
+++ src/app/auth/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  onLogin() {
+    console.log('Logging in with:', this.username, this.password);
+    // Here you would call your API to validate login
+  }
+}
