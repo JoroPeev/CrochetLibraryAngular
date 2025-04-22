87b24ab23b027f3aa91e9e6d0b1b0cac0cdf1c5b
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-22 20:09:44 +0300

    Added forms to main

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    FormsModule  // This enables ngModel for template-driven forms
   ]
 });
