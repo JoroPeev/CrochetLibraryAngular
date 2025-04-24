7eb1297de9cc7f1c5f328af692c8d2fe9d029ff8
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-04-24 19:55:59 +0300

    Removed Comments

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
-    FormsModule  // This enables ngModel for template-driven forms
+    FormsModule
   ]
 });
