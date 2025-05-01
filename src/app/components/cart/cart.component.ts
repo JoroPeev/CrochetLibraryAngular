6e4df30d8732353e6d2668957149a5d74e1377d6
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-01 23:03:22 +0300

    Added cart component

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {}
