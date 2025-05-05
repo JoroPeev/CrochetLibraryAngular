557ad99fe6c095dd416adfa1402ee29d425b7585
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-05 22:39:26 +0300

    Added toy product card html css and routing

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
+import { Toys } from '../../models/toys';
 @Component({
-  selector: 'app-home',
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent{}
+export class ProductCardComponent implements OnInit {
+  products: Toys[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getToys().subscribe(data => {
+      this.products = data;
+    });
+  }
+}
