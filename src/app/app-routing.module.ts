126fb5c472ab882524bdb5709188ec46acad4db9
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-05 22:36:58 +0300

    Added routing and removed product card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/about', pathMatch: 'full' }, // Default redirect
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  // Root redirects to Home page
+  { path: 'home', component: HomeComponent },  // Display HomeComponent for root
+  { path: 'shop', component: ShopComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
