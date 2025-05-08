af3d61fcfd8621a408e3baf4470a1b8a8f23549d
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-08 21:07:04 +0300

    Request handle product card

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ApiService } from '../../services/api.service';
+import { FormsModule } from '@angular/forms';
 import { Toys } from '../../models/toys';
+
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent implements OnInit {
-  products: Toys[] = [];
+export class ProductCardComponent {
+  @Input() product!: Toys;
+  showRequestModal = false;
 
-  constructor(private apiService: ApiService) {}
+  requestData = {
+    name: '',
+    email: '',
+    message: '',
+    dueDate: ''
+  };
 
-  ngOnInit() {
-    this.apiService.getToys().subscribe(data => {
-      this.products = data;
-    });
+  openRequestModal() {
+    this.showRequestModal = true;
+  }
+
+  closeRequestModal() {
+    this.showRequestModal = false;
+  }
+
+  submitRequest() {
+    console.log('Request submitted:', this.requestData);
+    alert(`Request sent for ${this.product.name}. Due: ${this.requestData.dueDate}`);
+    this.requestData = { name: '', email: '', message: '', dueDate: '' };
+    this.closeRequestModal();
   }
 }
