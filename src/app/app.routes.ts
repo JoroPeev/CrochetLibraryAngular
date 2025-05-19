2b6e069445cf2872089ee33759803d35fb856416
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-19 22:45:26 +0300

    Added to app routes login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { CartComponent } from './components/cart/cart.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,4 +16,5 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'shop', component: ProductListComponent },
+  { path: 'login', component: LoginComponent },
 ];
