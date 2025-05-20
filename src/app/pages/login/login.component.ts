30b1c55ba3cf490ab433a0293fa61fd3c88bfc9d
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-20 18:34:24 +0300

    Added imports for login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  @Output() close = new EventEmitter<void>();
+
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    const loginData = { email: this.email, password: this.password };
+    this.authService.login(loginData).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        alert('Login successful!');
+        this.close.emit();
+      },
+      error: () => alert('Invalid login.')
+    });
+  }
+}
