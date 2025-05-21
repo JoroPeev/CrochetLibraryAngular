54fa0cd6dcd2030f0a85ebf6650d3996c0a723f9
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-21 18:29:55 +0300

    Routed admin component and auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,9 @@ import { ProductDetailComponent } from './pages/product-detail/product-detail.co
 import { CartComponent } from './components/cart/cart.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
 import { LoginComponent } from './pages/login/login.component';
+
+import { AuthGuard } from './services/auth.guard'
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,6 +16,25 @@ export const routes: Routes = [
   { path: 'cart', component: CartComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'shop', component: ProductListComponent },
   { path: 'login', component: LoginComponent },
+
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./admin/pages/dashboard.component').then(m => m.DashboardComponent)
+      },
+      {
+        path: 'admin/products',
+        loadComponent: () => import('./admin/pages/products.component').then(m => m.ProductsComponent)
+      },
+      {
+        path: 'products/:id',
+        loadComponent: () => import('./admin/pages/product-edit.component').then(m => m.ProductEditComponent)
+      }
+    ]
+  }
 ];
