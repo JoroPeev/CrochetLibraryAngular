9e35b70da4db325703f33171f56f91f98e5a26e4
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-05-22 18:54:25 +0300

    Added to api services put delete methods

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -15,15 +15,22 @@ export class ApiService {
   getToys(): Observable<Toys[]> {
     return this.http.get<Toys[]>(this.apiUrl);
   }
-
-  // 🚀 Add this method here
   addRequest(requestData: any): Observable<any> {
     return this.http.post(this.requestsApiUrl, requestData);
-    
   }
   login(credentials: any): Observable<any> {
     return this.http.post('https://localhost:7298/api/auth/login', credentials);
   }
-  
+  addToy(toy: Toys): Observable<any> {
+    return this.http.post(this.apiUrl, toy);
+  }
+
+  updateToy(id: number, toy: Toys): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, toy);
+  }
+
+  deleteToy(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 
 }
