89ce7e3238ce8b907b84ebdb9b57ca912c0fa558
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-06-05 18:01:27 +0300

    Removed hero banner and home components

--- src/app/components/hero-banner/hero-banner.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './hero-banner.component.html',
-  styleUrls: ['./hero-banner.component.css']
-})
-export class HeroBannerComponent{}
