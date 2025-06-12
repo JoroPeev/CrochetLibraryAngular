598c227b09b40e7a74a80cff04971729f0b23378
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-06-12 19:12:12 +0300

    Added product detail ts file on init interface implemented

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent implements OnInit {
+  product: any = {};
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      console.log('Received query params:', params); // ✅ debug log
+      this.product = params;
+    });
+  }
+}
