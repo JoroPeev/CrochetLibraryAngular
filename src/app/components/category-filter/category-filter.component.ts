b7c3d66fe7ef090123310d20366e534fe3a16131
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-06-17 19:23:55 +0300

    Added functionality to category filter component

--- src/app/components/category-filter/category-filter.component.ts
+++ src/app/components/category-filter/category-filter.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common'; // 👈 Add this!
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-category-filter',
+  standalone: true, // 👈 if it’s standalone (which it should be for your setup)
+  imports: [CommonModule], // 👈 Add this!
   templateUrl: './category-filter.component.html',
   styleUrls: ['./category-filter.component.css']
 })
-export class CategoryFilterComponent{}
+export class CategoryFilterComponent {
+  @Input() categories: string[] = [];
+  @Output() categorySelected = new EventEmitter<string>();
+
+  activeCategory: string = 'All';
+
+  selectCategory(category: string) {
+    this.activeCategory = category;
+    this.categorySelected.emit(category);
+  }
+}
