e9d4ca6531991833e90faf8c721c3e16ed89c8bb
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-07-01 18:33:18 +0300

    Made toy id optional

--- src/app/models/toys.ts
+++ src/app/models/toys.ts
@@ -6,12 +6,12 @@ export interface ToyImage {
 }
 
 export interface Toy {
-  id: string;
+  id?: string; // Make ID optional for new toys
   name: string;
   description: string;
   price: number;
   colors: string;
   stock: number;
-  images: ToyImage[]; // lowercase i here
+  images: ToyImage[];
   primaryImageUrl: string;
 }
