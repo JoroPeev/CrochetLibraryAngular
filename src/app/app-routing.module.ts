d0b0da2eb60aa5c351e18674e4ca9d0b9a39e2c7
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-07-02 18:22:12 +0300

    Added route for shop page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component'; // ✅ import it here
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
@@ -10,12 +10,15 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'product-detail', component: ProductDetailComponent }
+  { path: 'product-detail', component: ProductDetailComponent },
+  {
+    path: 'shop',
+    loadComponent: () =>
+      import('./pages/shop/shop.component').then(m => m.ShopComponent)
+  }
 ];
-
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
