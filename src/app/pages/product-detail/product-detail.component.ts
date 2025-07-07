1510097f398b9794b2eec775bb7904ec018e24ca
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-07-07 18:01:52 +0300

    Product detail edited

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,20 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Toy } from '../../models/toys';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  imports: [CommonModule]
 })
-export class ProductDetailComponent implements OnInit {
-  product: any = {};
-
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      console.log('Received query params:', params); // ✅ debug log
-      this.product = params;
-    });
-  }
+export class ProductDetailComponent {
+  toy?: Toy;
 }
+
